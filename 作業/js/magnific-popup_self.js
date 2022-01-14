461df8db43d97dd9a0cee5d435fe712b606c3cac
BlackCat0708 <blackcat19980708@gmail.com>
2022-01-14 10:40:53 +0800

    上傳作業

---
+++ 作業/js/magnific-popup_self.js
@@ -0,0 +1,24 @@
+    // magnific-popup
+    $(document).ready(function () {
+      $('.zoom-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+          verticalFit: true,
+        },
+        gallery: {
+          enabled: true
+        },
+        zoom: {
+          enabled: true,
+          duration: 300, // don't foget to change the duration also in CSS
+          opener: function (element) {
+            return element.find('img');
+          }
+        }
+
+      });
+    });
